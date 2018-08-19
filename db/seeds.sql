0e238ea647e0f7511c832354cec7eb10d3164574
Alex Camacho <alex.c@WalkMe.local>
2018-08-19 15:32:27 -0700

    completed the DB setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Hickory burger',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Louisiana burger',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Hawaiian burger',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Western burger',false);
