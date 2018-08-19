76a8f86994b812dcab01a151a25f4d8ce9e86fcb
Alex Camacho <alex.c@WalkMe.local>
2018-08-19 16:23:45 -0700

    started the controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const burger = require('./models/burger.js');
+
+var router = express.Router();
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            cats: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+/* router.post("/api/cats", function (req, res) {
+    burger.insert([
+        "name", "sleepy"
+    ], [
+        req.body.name, req.body.sleepy
+    ], function (result) {
+        // Send back the ID of the new quote
+        res.json({
+            id: result.insertId
+        });
+    });
+});
+
+router.put("/api/cats/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        sleepy: req.body.sleepy
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+}); */
+
+// Export routes for server.js to use.
+module.exports = router;
